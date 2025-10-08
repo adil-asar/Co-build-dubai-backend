43df210c8a9496be5f980af665a3338dcdf39f7e
Adil-Ir <Dev.Adil@irsolutions.tech>
2025-10-08 16:08:20 +0500

    fixation

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
+// import app from "../src/server.js";
+// module.exports = (req, res) => {
+//   app(req, res);
+// };
 import app from "../src/server.js";
-module.exports = (req, res) => {
+
+export default function handler(req, res) {
   app(req, res);
-};
+}
